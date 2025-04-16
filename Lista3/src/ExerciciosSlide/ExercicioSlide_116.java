2a7fdbd47920f400f298c2b1145298112178a61a
JoaoLazaroSolutis <joao.lneto@solutis.com.br>
2025-04-16 10:32:41 -0300

    Subindo Readme e ExercicioSlide_116 refatorado

--- Lista3/src/ExerciciosSlide/ExercicioSlide_116.java
+++ Lista3/src/ExerciciosSlide/ExercicioSlide_116.java
@@ -3,24 +3,37 @@ package ExerciciosSlide;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class ExercicioSlide_116 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        List<String> listaNomeFamiliares = new ArrayList<>();
+        List<String> nomesFamiliares = obterNomesFamiliares();
+        exibirNomesFormatados(nomesFamiliares);
+    }
 
-        Scanner scNomes = new Scanner(System.in);
-        Scanner scNumeros = new Scanner(System.in);
+    private static int obterQuantidadeFamiliares() {
         System.out.println("Digite a quantidade de familiares que deseja declarar:");
-        int qtdFamiliares = scNumeros.nextInt();
-        for (int i = 1; i <= qtdFamiliares; i++) {
-            System.out.println("Digite o nome do familiar: ");
-            String nomeFamiliare = scNomes.nextLine();
-            listaNomeFamiliares.add(nomeFamiliare);
-        }
-        System.out.println("Exibição lista formatada: ");
-        listaNomeFamiliares.forEach(System.out::println);
-        System.out.println("Exibição em formato de lista: ");
-        System.out.println(listaNomeFamiliares);
+        return scanner.nextInt();
+    }
 
+    private static List<String> obterNomesFamiliares() {
+        int quantidadeFamiliares = obterQuantidadeFamiliares();
+        List<String> listaNomes = new ArrayList<>();
+        IntStream.rangeClosed(1, quantidadeFamiliares)
+                .forEach(i -> {
+                    System.out.println("Digite o nome do familiar " + i + ":");
+                    listaNomes.add(scanner.nextLine());
+                });
+        return listaNomes;
+    }
+
+    private static void exibirNomesFormatados(List<String> listaNomes) {
+        System.out.println("\nExibição lista formatada:");
+        listaNomes.forEach(System.out::println);
+        System.out.println("\nExibição em formato de lista:");
+        System.out.println(listaNomes);
     }
 }
